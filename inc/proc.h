f5dc2aa1fcd1894487c14c35196e8031717829c3
Sergei Krivchenko <s.krivchenko@metrotek.ru>
2024-10-20 18:46:44 +0300

    Useless commit

--- inc/proc.h
+++ inc/proc.h
@@ -6,7 +6,17 @@
 #include "rcv.h"
 #include "xmt.h"
 
-void rt_msg_processor(unsigned short* BASE_ADDR);
+#define RT_ADDRESS      (0x1F << 11)
+#define MSG_ERROR       (1    << 10)
+#define INSTR_BIT       (1    << 1)
+#define SERVICE_REQ     (1    << 5)
+#define BCST_RCVD       (1    << 4)
+#define BUSY            (1    << 3)
+#define SUBSYS_FLAG     (1    << 2)
+#define DYN_BUS_ACCEPT  (1    << 1)
+#define TERMINAL_FLAG   (1    << 0)
+
+void rt_msg_processor();
 
 #endif
 
--- src/proc.c
+++ src/proc.c
@@ -17,6 +17,7 @@ void transmit_fsm () {
 
   TRANSMIT_S:
     // DO STUFF
+    set_status_reg(0x0);
     WAIT_S;
 
   TRANSMIT_DATA_S:
@@ -41,6 +42,8 @@ int mode_code_reserved () {
     return 1;
   if((
     return 
+}
+
 void mode_code_fsm () {
 
   MODE_CODE_S:
@@ -134,7 +137,7 @@ void main_fsm () {
 }
 
 
-void rt_msg_processor(unsigned short* BASE_ADDR) {
+void rt_msg_processor() {
 
   rx_msg_info_t rx_msg_info;
   tx_msg_info_t tx_msg_info;
@@ -204,71 +207,3 @@ void rt_msg_processor(unsigned short* BASE_ADDR) {
 
 }
 
-////typedef struct {
-////  unsigned char   rt_address;
-////  unsigned char   msg_error;
-////  //unsigned char   instr_bit;
-////  //unsigned char   service_req;
-////  unsigned char   bcst_rcvd;
-////  //unsigned char   busy;
-////  //unsigned char   subsys_flag;
-////  //unsigned char   dyn_bus_accept;
-////  //unsigned char   terminal_flag;
-////} stat_data_t;
-////
-////typedef struct {
-////  unsigned char   msg_type; // 0 - COMMAND, 1 - DATA
-////  unsigned short  msg_data;
-////} tx_msg_info_t;
-//
-//void rt_msg_processor(){
-//
-//  rx_msg_info_t rx_msg_info;
-//  tx_msg_info_t tx_msg_info;
-//
-//  while(1){
-//
-//    receive_word(rx_msg_info);
-//
-//    if(rx_msg_info.cmd == 0){
-//      // Receive command
-//      if(rx_msg_info.mode_code){
-//        // Mode code command
-//        if(rx_msg_info.mode_code_wdata){
-//          receive_word();
-//        }
-//      } else {
-//        // Receive data command
-//        receive_word();
-//        if(rx_msg_info.cmd == 1){
-//          // RT - RT transaction
-//          receive_word();
-//        }
-//        unsigned char word_cnt = (rx_msg_info.word_cnt == 0) ? 32 : rx_msg_info.word_cnt;
-//        while(word_cnt){
-//          receive_word();
-//          word_cnt--;
-//        }
-//      }
-//      if(rx_msg_info.broadcast){
-//        // SKIP status word
-//      } else {
-//        wait_gap();
-//        send_status_word();
-//      }
-//    } else {
-//      // Transmit command
-//      wait_gap();
-//      send_status_word();
-//      if(rx_msg_info.mode_code){
-//        transmit_word();
-//      } else {
-//        unsigned char word_cnt = (rx_msg_info.word_cnt == 0) ? 32 : rx_msg_info.word_cnt;
-//        while(word_cnt){
-//          transmit_word();
-//          word_cnt--;
-//        }
-//      }
-//    }
-//}
-//
