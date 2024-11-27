9e1e5bfdc45c8ee6cc464a9904467d256b61983d
Sergei Krivchenko <s.krivchenko@metrotek.ru>
2024-11-27 10:35:20 +0300

    Alternative version of 'main' - FSM based - added.

--- src/main_fsm.c
+++ src/main_fsm.c
@@ -61,6 +61,10 @@ int main (){
         if(get_rx_word(&rx_word,10))
           break;
         msg_timestamp = get_timestamp_counter();
+        state = CHECK_MSG_ADDRESS;
+        break;
+
+      case CHECK_MSG_ADDRESS:
         if(rx_word.word_errors){
           break;
         }
@@ -69,24 +73,25 @@ int main (){
         if(!check_rt_address(&rx_word) && !check_broadcast_address(&rx_word)){
           break;
         }
-        state = CHECK_MSG_LEGALITY;
+        state = CHECK_MSG_ILLEGALITY;
         rx_ptr = 0;
         tx_ptr = 0;
         break;
 
-      case CHECK_MSG_LEGALITY:
+      case CHECK_MSG_ILLEGALITY:
+        pointer_table_addr  = (uint16_t*)(uintptr_t)(RT_MSG_PTR_TABLE_ADDR_GET() << 1);
+        pointer_table_addr += ((rx_word -> word_data & SUB_ADDR_MASK)>>SUB_ADDR_OFST);
         if(check_mode_code(&rx_word)){
           //msg_info.op_type = MODE_CODE_OP;
-          pointer_table_addr = (uint16_t*)(uintptr_t)(RT_MSG_PTR_TABLE_ADDR_GET() << 1)+MODE_BASE_INDX+((rx_word -> word_data & SUB_ADDR_MASK)>>SUB_ADDR_OFST);
+          pointer_table_addr += MODE_BASE_INDX;
         }
         else if(rx_word.word_data & DIR_MASK){
           //msg_info.op_type = TRANSMIT_OP;
-          pointer_table_addr = (uint16_t*)(uintptr_t)(RT_MSG_PTR_TABLE_ADDR_GET() << 1)+XMT_BASE_INDX+((rx_word -> word_data & SUB_ADDR_MASK)>>SUB_ADDR_OFST);
+          pointer_table_addr += XMT_BASE_INDX;
         } else {
           //msg_info.op_type = RECEIVE_OP;
-          pointer_table_addr = (uint16_t*)(uintptr_t)(RT_MSG_PTR_TABLE_ADDR_GET() << 1)+RCV_BASE_INDX+((rx_word -> word_data & SUB_ADDR_MASK)>>SUB_ADDR_OFST);
+          pointer_table_addr += RCV_BASE_INDX;
         }
-
         if(*pointer_table_addr == 0x0000){
           state = IDLE;
           break;
@@ -96,55 +101,50 @@ int main (){
 
       case GET_MSG_TABLE:
         msg_table_addr = (uint16_t*)(uintptr_t)(*pointer_table_addr << 1);
-        msg_table_size = (*msg_table_addr & MSG_TABLE_SIZE_MASK) >> MSG_TABLE_SIZE_OFST;
-
+        msg_table_size = (*msg_table_addr & MSG_TABLE_SIZE) >> MSG_TABLE_SIZE_OFST;
         if(msg_table_size == 0){
           state = IDLE;
           break;
         }
-        data_table_indx = (*msg_table_addr & MSG_TABLE_INDEX_MASK) >> MSG_TABLE_INDEX_OFST;
-        data_table_indx++;
-        if(data_table_indx > msg_table_size)
-          data_table_indx = 1;
-        state = GET_DATA_TABLE;
         msg_table_lock(msg_table_addr);
+        state = GET_DATA_TABLE;
         break;
 
       case GET_DATA_TABLE:
+        data_table_indx = (*msg_table_addr & MSG_TABLE_INDEX) >> MSG_TABLE_INDEX_OFST;
+        data_table_indx++;
+        if(data_table_indx > msg_table_size)
+          data_table_indx = 1;
         data_table_addr = (uint16_t*)(uintptr_t)(*msg_table_addr << 1) + data_table_indx;
         data_table_size = (*data_table_addr & DATA_TABLE_SIZE_MASK) >> DATA_TABLE_SIZE_OFST;
-
         if(data_table_size == 0){
           state = IDLE;
           break;
         }
-        state = PARSE_MSG;
         data_table_lock(data_table_addr);
+        state = PARSE_MSG;
         break;
 
       case PARSE_MSG:
-
         msg_info.broadcast = check_broadcast_address(&rx_word);
         msg_info.words_cnt = (rx_word -> word_data & WORD_COUNT_MASK);
         msg_info.mode_code = (rx_word -> word_data & MODE_CODE_MASK);
-
-        if(msg_info.words_cnt == 0)
+        if(msg_info.words_cnt == 0){
           msg_info.words_cnt = 32;
-
+        }
         if(check_mode_code(&rx_word)){
           msg_info.op_type = MODE_CODE_OP;
           state = MODE_CODE;
-        }
-        else if(rx_word.word_data & DIR_MASK){
+        } else if(rx_word.word_data & DIR_MASK){
           msg_info.op_type = TRANSMIT_OP;
           state = RESPONSE;
         } else {
           msg_info.op_type = RECEIVE_OP;
-          state = RECEIVE;
+          state = RECEIVE_DATA;
         }
         break;
 
-      case RECEIVE:
+      case RECEIVE_DATA:
         if(get_rx_word(&rx_word,4)){
           state = IDLE;
           break;
@@ -166,37 +166,133 @@ int main (){
         break;
 
       case MODE_CODE:
+        state = IDLE;
         switch(rx_word -> word_data & MODE_CODE_MASK){
 
           case CMD_DYNAMIC_BUS_CONTROL:
-            if(broadcast){
-              // Reset transaction
-              return;
-            }
+            if(broadcast)
+              break;
             do_cmd_dynamic_bus_control();
             send_tx_word(0,BASIC_STATUS_GET());
             break;
 
+          case CMD_SYNCHRONIZE:
+            do_cmd_synchronize();
+            if(broadcast)
+              break;
+            send_tx_word(0,BASIC_STATUS_GET());
+            break;
+
+          case CMD_TRANSMIT_STATUS_WORD:
+            if(broadcast)
+              break;
+            do_cmd_transmit_status_word();
+            send_tx_word(0,BASIC_STATUS_GET());
+            break;
+
+          case CMD_INITIATE_SELF_TEST:
+            do_cmd_initiate_self_test();
+            if(broadcast)
+              break;
+            send_tx_word(0,BASIC_STATUS_GET());
+            break;
+
+          case CMD_TRANSMITTER_SHUTDOWN:
+            do_cmd_transmitter_shutdown();
+            if(broadcast)
+              break;
+            send_tx_word(0,BASIC_STATUS_GET());
+            break;
+
+          case CMD_OVERRIDE_TRANSMITTER_SHUTDOWN:
+            do_cmd_override_transmitter_shutdown();
+            if(broadcast)
+              break;
+            send_tx_word(0,BASIC_STATUS_GET());
+            break;
+
+          case CMD_INHIBIT_TERMINAL_FLAG_BIT:
+            do_cmd_inhibit_terminal_flag_bit();
+            if(broadcast)
+              break;
+            send_tx_word(0,BASIC_STATUS_GET());
+            break;
+
+          case CMD_OVERRIDE_INHIBIT_TERMINAL_FLAG_BIT:
+            do_cmd_override_inhibit_terminal_flag_bit();
+            if(broadcast)
+              break;
+            send_tx_word(0,BASIC_STATUS_GET());
+            break;
+
+          case CMD_RESET_REMOTE_TERMINAL:
+            do_cmd_reset_remote_terminal();
+            if(broadcast)
+              break;
+            send_tx_word(0,BASIC_STATUS_GET());
+            break;
+
+          case CMD_TRANSMIT_VECTOR_WORD:
+            if(broadcast)
+              break;
+            response_gap(12);
+            send_tx_word(0,BASIC_STATUS_GET());
+            send_tx_word(1,0xFFFF);
+            break;
+
+          case CMD_SYNCHRONIZE_WITH_DATA_WORD:
+            if(get_rx_word(&rx_word,4))
+              break;
+            do_cmd_synchronize_with_data_word();
+            break;
+
+          case CMD_TRANSMIT_LAST_COMMAND_WORD:
+            if(broadcast)
+              break;
+            response_gap(12);
+            send_tx_word(0,BASIC_STATUS_GET());
+            send_tx_word(1,0xFFFF);
+            break;
+
+          case CMD_TRANSMIT_BUILT_IN_TEST_WORD:
+            if(broadcast)
+              break;
+            response_gap(12);
+            send_tx_word(0,BASIC_STATUS_GET());
+            send_tx_word(1,0xFFFF);
+            break;
+
+          case CMD_SELECTED_TRANSMITTER:
+            if(get_rx_word(&rx_word,4))
+              break;
+            do_cmd_selected_transmitter();
+            break;
+
+          case CMD_OVERRIDE_SELECTED_TRANSMITTER:
+            if(get_rx_word(&rx_word,4))
+              break;
+            do_cmd_override_selected_transmitter();
+            break;
         }
 
-      case RESPONSE:
-        response_gap(12);
-        send_tx_word(0,BASIC_STATUS_GET());
-        if(msg_info.op_type == TRANSMIT_OP)
-          state = TRANSMIT;
-        else
-          if(msg_info.op_type == MODE_CODE_OP && response_enable)
-            state = MODE_CODE;
-          else
-            state = FINISH;
+      case RECEIVE_RESPONSE:
+        check_minimum_response_time();
+        if(get_rx_word(&rx_word,4))
+          break;
+        state = RECEIVE_DATA;
         break;
 
-      case TRANSMIT:
+      case TRANSMIT_RESPONSE:
+        response_gap(12);
+        send_tx_word(0,BASIC_STATUS_GET());
+        state = TRANSMIT_DATA;
+        break;
+
+      case TRANSMIT_DATA:
         send_tx_word(1,*(data_table_addr+3+tx_ptr++));
-        if(tx_ptr == words_cnt){
+        if(tx_ptr == words_cnt)
           state = FINISH;
-          break;
-        }
+        break;
 
       case FINISH:
         if(msg_info.op_type == RECEIVE_OP)
@@ -228,7 +324,6 @@ int check_broadcast_address(rx_word_t* rx_word){
   return ((rx_word -> word_data & RT_ADDR_MASK) == BROADCAST_ADDR);
 }
 
-
 int check_mode_code(rx_word_t* rx_word){
   if((rx_word -> word_data & SUB_ADDR_MASK) == MODE_CODE_ADDR0)
     return 1;
@@ -236,3 +331,8 @@ int check_mode_code(rx_word_t* rx_word){
     return 1;
   return 0;
 }
+
+int check_minimum_response_time(){
+  // NOP
+}
+
